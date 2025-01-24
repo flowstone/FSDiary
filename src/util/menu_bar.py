bd710a3e28fd06af301785d110ce40fcdc5bef1c
simonxue <xueyao.me@gmail.com>
2025-01-25 02:36:57 +0800

    feat-加入日志窗口

--- src/util/menu_bar.py
+++ src/util/menu_bar.py
@@ -5,6 +5,7 @@ from src.about_window import AboutWindow
 from src.const.fs_constants import FsConstants
 import os
 
+from src.log_window import LogWindow
 from src.option_general import OptionGeneral
 from src.option_tab import OptionTab
 from src.util.common_util import CommonUtil
@@ -16,7 +17,8 @@ class MenuBar:
     def __init__(self, parent):
         self.parent = parent
         self.menu_bar = QMenuBar(self.parent)
-
+        # 初始化日志窗口
+        self.log_window = LogWindow()
         # 初始化首选项窗口
         self.option_tab = OptionTab()
         self._create_help_menu()
@@ -30,6 +32,9 @@ class MenuBar:
         # 创建首选项菜单项
         option_tab_action = QAction("首选项", self.parent)
         option_tab_action.triggered.connect(self.show_option_tab)
+        # 创建日志窗口菜单项
+        open_log_action = QAction("日志", self.parent)
+        open_log_action.triggered.connect(self.show_log_window)
 
         # 创建日志窗口菜单项
         about_action = QAction("关于", self.parent)
@@ -37,6 +42,7 @@ class MenuBar:
 
         # 将菜单项添加到“帮助”菜单中
         help_menu.addAction(option_tab_action)
+        help_menu.addAction(open_log_action)
         help_menu.addAction(about_action)
 
 
@@ -48,6 +54,9 @@ class MenuBar:
 
 
 
+    def show_log_window(self):
+        """显示日志窗口"""
+        self.log_window.show()
 
     def show_option_tab(self):
         """显示首选项窗口"""
